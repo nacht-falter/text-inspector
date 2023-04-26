4bbac15ac85e5110c7ab11724ce2adecfc26848b
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-04-26 13:53:16 +0200

    Add function placeholders and create main menu

--- run.py
+++ run.py
@@ -30,8 +30,8 @@ class Menu:
                 # https://stackoverflow.com/questions/251464/how-to-get-a-function-name-as-a-string
                 # https://stackoverflow.com/questions/34277363/how-to-print-your-functions-documentation-python
                 """
-                menu_item = f"{item.__name__}: {item.__doc__}"
-                print(f'{i}. {menu_item.capitalize().replace("_", " ")}')
+                menu_item = f'{item.__name__.capitalize().replace("_", " ")}: {item.__doc__}'
+                print(f"{i}. {menu_item}")
                 i += 1
             print(f"{i}. Exit")
             option = int(input("\nPlease choose an option: "))
@@ -46,3 +46,21 @@ class Menu:
             except ValueError:
                 print(f"Invalid choice. Please enter a number between 1 and {len(self.menu_items)}.\n")
                 time.sleep(2)
+
+def spell_check():
+    """Check for spelling errors in the selected text"""
+    print("Spell check")
+    time.sleep(1)
+
+def suggest_synonyms():
+    """Suggest synonyms for frequently used words"""
+    print("synonyms")
+    time.sleep(1)
+
+def display_metrics():
+    """Display metrics for the seleced text"""
+    print("metrics")
+    time.sleep(1)
+
+main_menu = Menu("Main menu", spell_check, suggest_synonyms, display_metrics)
+main_menu.display_menu()
