0fbb9b515cc29b8aec2fd4cd8bdcb83fd607125a
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-04-28 11:23:10 +0200

    Implement text metrics feature

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from enum import unique
 import os
 import re
 import time
@@ -29,6 +30,8 @@ class Text:
     def __init__(self):
         self.title = self.get_title()
         self.text = self.get_text()
+        # Split text into list with words and punctuation: https://stackoverflow.com/questions/367155/splitting-a-string-into-words-and-punctuation
+        self.split_text = re.findall(r"[\w'-]+|[ .,!?@#$%&*;:<>=()[\]{}\n]", self.text)
 
     def get_title(self):
         """Get instance title from user input"""
@@ -104,19 +107,16 @@ class Text:
 
     def spell_check(self):
         """Check for spelling errors in the selected text"""
-
-        # Split text into list with words and punctuation: https://stackoverflow.com/questions/367155/splitting-a-string-into-words-and-punctuation
-        split_text = re.findall(r"[\w']+|[ .,!?-_@#$%&*;:<>=()[\]{}\n]", self.text)
-
         # pyspellchecker documentation: https://pyspellchecker.readthedocs.io/en/latest/
         spell = SpellChecker(language="en")
-        misspelled = spell.unknown(split_text)
+        misspelled = spell.unknown(self.split_text)
         time.sleep(2)
         corrected_text = []
         i = 0
 
-        for word in split_text:
-            if word.isalnum() and word in misspelled:
+        for word in self.split_text:
+            # match alphanumeric words including hyphens and underscores: https://stackoverflow.com/questions/34916716/regular-expression-to-match-alphanumeric-hyphen-underscore-and-space-string
+            if re.match(r"^[a-zA-Z]([\w-]*[a-zA-Z])?$", word) and word in misspelled:
                 suggestions = spell.candidates(word)
                 # Call display_suggestions method and pass it the current word and its suggestions
                 corrected_text.append(self.display_suggestions(word, suggestions))
@@ -185,7 +185,41 @@ class Text:
 
     def display_metrics(self):
         """Display metrics for the seleced text"""
-        print("metrics")
+        display_header()
+        print(f"Selected Text: {self.title}\n")
+        print("Text Metrics:")
+
+        # Total word count and unique words:
+        total_words = 0
+        unique_words = {}
+        for word in self.split_text:
+            if re.match(r"^[a-za-z]([\w-]*[a-za-z])?$", word):
+                total_words += 1
+                if word in unique_words:
+                    unique_words[word] += 1
+                else:
+                    unique_words[word] = 1
+
+        # Total sentence count: https://stackoverflow.com/questions/15228054/how-to-count-the-amount-of-sentences-in-a-paragraph-in-python
+        all_sentences = re.split(r"[.!?]+", self.text)
+        total_sentences = len(all_sentences)
+
+        # Longest/shortest sentences:
+        sentence_lengths = set()
+        for sentence in all_sentences:
+            words = re.findall(r"\w+", sentence)
+            if len(words) > 0:
+                sentence_lengths.add(len(words))
+
+        # Average words per sentence:
+        words_per_sentence = round(total_words / total_sentences)
+
+        print(f"Words: {total_words}")
+        print(f"Sentences: {total_sentences}")
+        print(f"Longest sentence: {max(sentence_lengths)} words")
+        print(f"Shortest sentence: {min(sentence_lengths)} words")
+        print(f"Average words per sentence: {words_per_sentence}")
+
         input("\nPress Enter to return to menu.")
 
 
@@ -275,7 +309,7 @@ def select_text():
 
 
 def create_new_text():
-    """Create a new text from Text class"""
+    """Create a new text from command line input or from file"""
     new_text = Text()
 
     return new_text
